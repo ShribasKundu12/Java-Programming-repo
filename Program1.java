9b57fb63441d5f83fd0c83e05e2626ecbdcb16f6
Shribas Kundu <147904187+ShribasKundu12@users.noreply.github.com>
2024-04-23 21:35:08 +0530

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,8 @@
+//Day01OfJavaProgramming
+public class Program1{
+	//main method
+	public static void main(String args[]){
+		//print statement
+		System.out.println("This Is My First Java Code");
+	}
+}
