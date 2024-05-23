40643796a333c76a3a3a02c726c9703a645f88c5
Shribas Kundu <147904187+ShribasKundu12@users.noreply.github.com>
2024-05-23 23:51:05 +0530

    Add files via upload

---
+++ DemoPac1.java
@@ -0,0 +1,20 @@
+//to create a package we will use package keyword
+
+package demopac;
+public class DemoPac1{
+	public static void main(String[] s){
+
+		System.out.println("I'm DemoPac1");
+				
+	}
+	
+
+}
+
+/*for compiling the package:
+C:shrib\JAVA FOLDER>javac -d . DemoPac1.java
+
+to run DemoPac1.java file:
+C:shrib\JAVA FOLDER>java demopac.DemoPac1
+*/
+
